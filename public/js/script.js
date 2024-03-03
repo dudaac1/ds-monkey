f4bc0572b092300cb2850bf2e545914a4ae0f61b
jjuliar <juliarochajunqueira@gmail.com>
2024-03-03 15:37:38 -0300

    changes in chat layout

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,49 @@
 const userInput = document.getElementById('userInput');
 const chatLog = document.getElementById('chatLog');
+var prompt = "Seu nome é Luna. Você é uma assistente virtual que responde perguntas sobre sustentabilidade e consumo energético. Não escreva de modo itemizado. Responda de modo breve e direto, utilizando no máximo 100 palavras: \n"
+
+var i = 0;
+let speed;
+function typeWriter(response, p) {
+  speed = 50;
+  
+  if (i < response.length) {
+    p.innerHTML += response.charAt(i);
+    i++;
+    setTimeout(() => typeWriter(response, p), speed);
+  } else {
+    document.getElementById("sendButton").disabled = false;
+  }
+}
 
 async function sendMessage() {
   userInputValue = userInput.value;
-  // console.log(userInputValue);
   userInput.value = '';
 
   var p = document.createElement('p');
-  p.innerHTML = "<b>YOU:</b><br>" + userInputValue;
+  p.innerHTML = "<b>VOCÊ:</b><br>" + userInputValue;
   p.classList = 'sent';
   chatLog.appendChild(p);
 
   const response = await generateResponse(userInputValue);
-  displayResponse(await response);
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      displayResponse(response);
+      resolve();
+    }, userInputValue.length * speed); // Esse cálculo baseia-se no tempo que a função typeWriter levará para digitar toda a entrada
+  });
+  
 }
 
 async function generateResponse(inputText) {
+  
   const loader = document.querySelector('.loader');
+  document.getElementById("sendButton").disabled = true; 
   loader.style.display = 'inline-block'; // Mostra o loader
     const params = {
-        messages: [{ "role": "user", "content": inputText }],
+        messages: [{ "role": "user", "content": prompt + inputText }],
         do_sample: true,
-        max_tokens: 200,
+        max_tokens: 300,
         temperature: 0.2,
         top_p: 0.75,
     };
@@ -31,7 +53,7 @@ async function generateResponse(inputText) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Key chave' // Substitua YOUR_API_KEY pelo seu próprio token de autenticação
+            'Authorization': 'Key your_key' // Substitua YOUR_API_KEY pelo seu próprio token de autenticação
         },
         body: JSON.stringify(params)
     });
@@ -48,7 +70,9 @@ async function generateResponse(inputText) {
 
 function displayResponse(response) {
   var p = document.createElement('p');
-  p.innerHTML = "<b>LUNA:</b><br>" + response;
-  p.classList = 'received';
+  p.innerHTML = "<b>LUNA:</b><br>";
+  p.classList = 'text-incoming received';
   chatLog.appendChild(p);
+  i = 0;
+  typeWriter(response, p); 
 }
